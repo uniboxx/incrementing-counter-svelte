7d1fdf743082d71782586d43a353d01b82336c8d
uniboxx <unibox@duck.com>
2024-08-17 16:41:53 +0200

    finish project

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte';
+</script>
+
+<Counter icon="twitter" text="12000" span="Twitter Followers" />
+<Counter icon="youtube" text="5000" span="Youtube Subscribers" />
+<Counter icon="facebook" text="7500" span="Facebook Fans" />
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { onMount } from 'svelte';
+  let { icon, text, span } = $props();
+  let counter = $state(0);
+
+  const increment = +text / 700;
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      if (counter > +text) {
+        counter = text;
+        clearInterval(interval);
+      } else counter = Math.ceil(counter + increment);
+    }, 1);
+  });
+</script>
+
+<div class="counter-container">
+  <i class={`fab fa-3x fa-${icon}`}></i>
+  <div class="counter">{counter}</div>
+  <div id={span}></div>
+</div>
+
+<style lang="scss">
+  .counter-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    flex: 1;
+
+    .counter {
+      font-size: 60px;
+      margin-top: 10px;
+    }
+    span {
+      white-space: nowrap;
+    }
+  }
+</style>
